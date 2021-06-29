5940011c9b80f6fd51dfe7fb6ba0b6efa89ddb60
Кристина <kristina.ezikova@yandex.ru>
2021-06-30 01:36:05 +0300

    Method tests

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 import requests
 
-response = requests.post("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+url = "https://playground.learnqa.ru/ajax/api/compare_query_type"
 
-print(response.url)
-print(response.history)
-print(len(response.history))
+# Wrong method provided
+response_without_params = requests.get(url)
+print(response_without_params.text)
+
+# {"success":"!"}
+response_with_params = requests.get(url, params={"method": "GET"})
+print(response_with_params.text)
+
+# Wrong method provided
+response_with_incorrect_params = requests.get(url, params={"method": "HEAD"})
+print(response_with_incorrect_params.text)
+
+methods = ["GET", "POST", "PUT", "DELETE"]
+print(methods)
+
+for method in methods:
+    if method == "GET":
+        response = requests.get(url, params={"method": method})
+        print("method="+method+" "+response.text)
+        response = requests.post(url, params={"method": method})
+        print("method="+method+" "+response.text)
+        response = requests.put(url, params={"method": method})
+        print("method="+method+" "+response.text)
+        response = requests.delete(url, params={"method": method})
+        print("method="+method+" "+response.text)
+    else:
+        response = requests.get(url, data={"method": method})
+        print("method="+method+" "+response.text)
+        response = requests.post(url, data={"method": method})
+        print("method="+method+" "+response.text)
+        response = requests.put(url, data={"method": method})
+        print("method="+method+" "+response.text)
+        response = requests.delete(url, data={"method": method})
+        print("method="+method+" "+response.text)
