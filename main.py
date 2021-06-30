a1be38d64f710dcd742f9c6fa194ddb629e12d58
Кристина <kristina.ezikova@yandex.ru>
2021-06-30 19:57:59 +0300

    Create task

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
 import requests
+import json
+import time
 
-url = "https://playground.learnqa.ru/ajax/api/compare_query_type"
+url = "https://playground.learnqa.ru/ajax/api/longtime_job"
 
-# Wrong method provided
-response_without_params = requests.get(url)
-print(response_without_params.text)
 
-# {"success":"!"}
-response_with_params = requests.get(url, params={"method": "GET"})
-print(response_with_params.text)
+def get_status(auth_token, expected_status, flag):
+    response_text = requests.get(url, params={"token": auth_token})
+    json_text = json.loads(response_text.text)
+    print(json_text)
+    print(flag)
 
-# Wrong method provided
-response_with_incorrect_params = requests.get(url, params={"method": "HEAD"})
-print(response_with_incorrect_params.text)
+    if flag:
+        try:
+            print(json_text["result"])
+        except Exception:
+            print('Cannot find a result')
+            exit()
 
-methods = ["GET", "POST", "PUT", "DELETE"]
-print(methods)
+    try:
+        if json_text["status"] != expected_status:
+            print("Incorrect status. Status code should be '" + expected_status + "'")
+            print(json_text["status"])
+            exit()
+    except Exception:
+        print('Cannot find a status value')
+    return json_text["status"]
 
-for method in methods:
-    if method == "GET":
-        response = requests.get(url, params={"method": method})
-        print("method="+method+" "+response.text)
-        response = requests.post(url, params={"method": method})
-        print("method="+method+" "+response.text)
-        response = requests.put(url, params={"method": method})
-        print("method="+method+" "+response.text)
-        response = requests.delete(url, params={"method": method})
-        print("method="+method+" "+response.text)
-    else:
-        response = requests.get(url, data={"method": method})
-        print("method="+method+" "+response.text)
-        response = requests.post(url, data={"method": method})
-        print("method="+method+" "+response.text)
-        response = requests.put(url, data={"method": method})
-        print("method="+method+" "+response.text)
-        response = requests.delete(url, data={"method": method})
-        print("method="+method+" "+response.text)
+
+response = requests.get(url)
+if response.status_code == 200:
+    response_json_text = json.loads(response.text)
+    try:
+        token = response_json_text["token"]
+
+        try:
+            seconds = response_json_text["seconds"]
+
+            print(get_status(token, "Job is NOT ready", 0))
+
+            time.sleep(seconds)
+
+            print(get_status(token, "Job is ready", 1))
+
+        except Exception:
+            print('Cannot find a seconds value')
+
+    except Exception:
+        print('Cannot find a token value')
+
+else:
+    print("Something went wrong")
