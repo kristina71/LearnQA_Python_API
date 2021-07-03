4da93f47acab140a266d09a1d3c1a02fd1db8c07
Кристина <kristina.ezikova@yandex.ru>
2021-07-03 23:35:20 +0300

    Header test

---
+++ test_header.py
@@ -0,0 +1,13 @@
+import pytest
+import requests
+
+class TestHeader:
+    def test_header(self):
+        url = "https://playground.learnqa.ru/api/homework_header"
+        response = requests.get(url)
+
+        assert "x-secret-homework-header" in response.headers, "There is no secret homework header in the response"
+        print(response.headers["x-secret-homework-header"])
+        assert "Some secret value" in response.headers["x-secret-homework-header"], "There is no some " \
+                                                                                    "secret value in the " \
+                                                                                    "header "
